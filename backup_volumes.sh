fb2f82d60b632b1be5c239e8e83fccf3ebdf4455
Francesco Pompò <pfrankw@users.noreply.github.com>
2024-07-30 18:10:01 +0200

    Create backup_volumes.sh

---
+++ backup_volumes.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+myself() {
+  hostname
+}
+
+volumes() {
+  # Inspect own container to find out which volumes are actually mounted at /volumes and print their name
+  docker inspect $(myself) --format '{{json .Mounts}}' | jq -r ".[] | select(.Destination | startswith(\"/volumes\")) | .Name"
+}
+
+get_mounts() {
+  cts=$(docker ps -q)
+
+  # For every volume passed as argument
+  for volume in $@; do
+    # For every running container
+    for ct in $cts; do
+      # Inspect it and determine if mounts a specific volume by its name
+      docker inspect "$ct" --format "{{json .Mounts}}" | jq -e -r ".[] | select(.Name == \"$volume\")" > /dev/null && echo "$volume $ct"
+      # jq -e makes jq exit with non-zero if there is no output, so the only printed containers are the ones that mount the volume
+    done
+  done
+}
+
+backup() {
+  volumes=$(volumes)
+
+  # Getting all mounts for all volumes that are under /volumes in my own container |
+  # Getting only the container IDs |
+  # uniq |
+  # Removing myself from the result
+
+  # This way we get only the containers that are running and of which we are mounting the volumes under /volumes
+
+  cts=$(get_mounts "$volumes" | cut -d ' ' -f 2 | uniq | grep -v "$(myself)")
+
+  echo "Stopping containers"
+  docker stop $cts
+
+  $COMMANDS_PRE
+
+  echo "Backing up /volumes"
+  restic backup /volumes
+
+  if [ $? -eq 0 ]; then
+    $COMMANDS_SUCCESS
+  else
+    $COMMANDS_FAIL
+  fi
+
+  $COMMANDS_POST
+
+  echo "Starting containers"
+  docker start $cts
+  docker start $cts # Sometimes containers have dependencies on others and wont start at the first time
+}
+
+backup
